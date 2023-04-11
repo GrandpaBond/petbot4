b7baba4f48fd5a5ca609d7fd75eb283f29f73930
GrandpaBond <89019204+GrandpaBond@users.noreply.github.com>
2023-04-11 08:06:06 +0100

    blinking sorted.

--- main.ts
+++ main.ts
@@ -92,6 +92,10 @@ function maybe_switch () {
             next_switch = input.runningTime() + randint(switch_gap, switch_vary * switch_gap)
         }
     }
+    if (my_mood == zMOOD_BORED || my_mood == zMOOD_ASLEEP) {
+        switch_gap = Math.constrain(Math.map(energy, -100, 100, 500, 5000), 500, 5000)
+        switch_time = Math.constrain(Math.map(energy, -100, 100, 5000, 500), 500, 5000)
+    }
 }
 input.onSound(DetectedSound.Loud, function () {
     excite(zMOOD_GOSH, 300)
@@ -158,21 +162,21 @@ input.onGesture(Gesture.ThreeG, function () {
 })
 function new_mood (mood: number) {
     if (mood == zMOOD_SNORING) {
-        set_mood(zEYES_SHUT, zMOUTH_FLAT, zEYES_SHUT, zMOUTH_OPEN, 2000, 2000, 1)
+        set_mood(zEYES_SHUT, zMOUTH_FLAT, zEYES_SHUT, zMOUTH_OPEN, 2000, 2000, 0)
     } else if (mood == zMOOD_ASLEEP) {
-        set_mood(zEYES_SHUT, zMOUTH_FLAT, zEYES_SHUT, zMOUTH_HMMM, 3000, 500, 1)
+        set_mood(zEYES_SHUT, zMOUTH_FLAT, zEYES_SHUT, zMOUTH_HMMM, 3000, 500, 0)
     } else if (mood == zMOOD_BORED) {
-        set_mood(zEYES_OPEN, zMOUTH_FLAT, zEYES_SHUT, zMOUTH_FLAT, 3000, 500, 4)
+        set_mood(zEYES_OPEN, zMOUTH_FLAT, zEYES_SHUT, zMOUTH_FLAT, 3000, 500, 3)
     } else if (mood == zMOOD_HAPPY) {
-        set_mood(zEYES_OPEN, zMOUTH_GRIN, zEYES_SHUT, zMOUTH_GRIN, 1000, 200, 3)
+        set_mood(zEYES_OPEN, zMOUTH_GRIN, zEYES_SHUT, zMOUTH_GRIN, 1000, 200, 2)
     } else if (mood == zMOOD_SAD) {
-        set_mood(zEYES_SAD, zMOUTH_SULK, zEYES_SHUT, zMOUTH_SULK, 4000, 500, 2)
+        set_mood(zEYES_SAD, zMOUTH_SULK, zEYES_SHUT, zMOUTH_SULK, 4000, 500, 1)
     } else if (mood == zMOOD_ANGRY) {
-        set_mood(zEYES_MAD, zMOUTH_HMMM, zEYES_MAD, zMOUTH_SHOUT, 2000, 800, 2)
+        set_mood(zEYES_MAD, zMOUTH_HMMM, zEYES_MAD, zMOUTH_SHOUT, 2000, 800, 1)
     } else if (mood == zMOOD_GOSH) {
-        set_mood(zEYES_POP, zMOUTH_OPEN, zEYES_POP, zMOUTH_OPEN, 1000, 1000, 1)
+        set_mood(zEYES_POP, zMOUTH_OPEN, zEYES_POP, zMOUTH_OPEN, 1000, 1000, 0)
     } else if (mood == zMOOD_SHIVER) {
-        set_mood(zEYES_LEFT, zMOUTH_SHOUT, zEYES_RIGHT, zMOUTH_OPEN, 200, 200, 1)
+        set_mood(zEYES_LEFT, zMOUTH_SHOUT, zEYES_RIGHT, zMOUTH_OPEN, 200, 200, 0)
     } else if (mood == zMOOD_DEAD) {
         basic.showIcon(IconNames.Skull)
     }
@@ -181,6 +185,7 @@ function new_mood (mood: number) {
         show_eyes(my_eyes)
         show_mouth(my_mouth)
     }
+    next_mood = mood
     serial.writeValue("new mood", my_mood)
     serial.writeLine("")
 }
